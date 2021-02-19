42177894daae57c040bdbe9eace827ff8b9ec84e
gare15 <72866876+gare15@users.noreply.github.com>
2021-02-19 21:01:49 +0700

    Add files via upload

---
+++ UAS-IF10K-10119911-MochamadTegar-no5/src/uas/if10k/pkg10119911/mochamadtegar/no5/UASIF10K10119911MochamadTegarNo5.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uas.if10k.pkg10119911.mochamadtegar.no5;
+
+
+/**
+ *
+ * @author Administrator
+ */
+public class UASIF10K10119911MochamadTegarNo5 {
+
+    /**
+     * @param args the command line arguments
+     */
+        mahasiswa m = new mahasiswa();
+
+    m.setNim("10110269");
+    m.setNama("Rizki Adam Kurniawan");
+
+
+    System.out.println("Nim Mahasiswa : " + m.setNim());
+    System.out.println("Nama Mahasiswa : " + m.setNama()); 
+    }
---
+++ UAS-IF10K-10119911-MochamadTegar-no5/src/uas/if10k/pkg10119911/mochamadtegar/no5/mahasiswa.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uas.if10k.pkg10119911.mochamadtegar.no5;
+
+/**
+ *
+ * @author Administrator
+ */
+public class mahasiswa {
+    private String nim;
+    private String nama;
+
+    // ini method setter
+    public void setNim(String nim){
+        this.nim = nim;
+    }
+
+    public void setNama(String nama){
+        this.nama = nama;
+    }
+
+    // ini method getter
+    public String getNim(){
+        return this.nim;
+    }
+
+    public String getNama(){
+        return this.nama;
+    }
+}
