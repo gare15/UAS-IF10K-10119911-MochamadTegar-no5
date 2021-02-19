3f585e59ec2a2f980ac179261425f08746c5b266
gare15 <72866876+gare15@users.noreply.github.com>
2021-02-19 21:39:07 +0700

    Update User.java

--- UAS-IF10K-10119911-MochamadTegar-no5/src/uas/if10k/pkg10119911/mochamadtegar/no5/User.java
+++ UAS-IF10K-10119911-MochamadTegar-no5/src/uas/if10k/pkg10119911/mochamadtegar/no5/User.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package percobaan1;
+package uas.if10k.pkg10119911.mochamadtegar.no5;
 
 /**
  *
